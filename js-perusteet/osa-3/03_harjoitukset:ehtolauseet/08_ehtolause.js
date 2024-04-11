bd1f019fa011c52928bc36bbbf95322c3a124e0a
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-04-11 12:56:26 +0300

    tauko

--- js-perusteet/osa-3/03_harjoitukset:ehtolauseet/08_ehtolause.js
+++ js-perusteet/osa-3/03_harjoitukset:ehtolauseet/08_ehtolause.js
@@ -7,8 +7,10 @@ Palauttaa merkkijonon, joka yhdistää numeron ja sopivasti taipuvan muodon subs
  */
 
 function pluralize(substantiivi, määrä) {
-  if (määrä >= 0) {
-    return määrä + substantiivi;
+  if (määrä === 0) {
+    return `${määrä} ${substantiivi}` + "a";
+  } else if (määrä >= 1) {
+    return `${määrä} ${substantiivi}` + "a";
   }
 }
 
@@ -18,5 +20,16 @@ console.log("Minulla on " + pluralize("koira", 1));
 console.log("Minulla on " + pluralize("papukaija", 7));
 
 // Treenaa: Keksi itse samanlainen funktio eri nimellä.
-
+function monikko(esine, kpl) {
+  if (kpl === 1) {
+    return `${kpl} ${esine}` + "n";
+  } else if (kpl <= 2) {
+    return `${kpl} ${esine}` + "tta";
+  } else if (kpl >= 4) {
+    return `${kpl} ${esine}` + "a";
+  }
+}
 // Kutsu tekemääsi funktiota
+console.log("Omistan " + monikko("auto", 1));
+console.log("Omistan " + monikko("lentokone", 2));
+console.log("Omistan " + monikko("Asunto", 4));

3b740c1506234425c50a7b8fb84bd571987228aa
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-04-18 10:45:59 +0300

    päivit

--- js-perusteet/osa-03/03_harjoitukset/silmukat/04_silmukat.js
+++ js-perusteet/osa-03/03_harjoitukset/silmukat/04_silmukat.js
@@ -2,15 +2,13 @@
 //ohjelman tulisi näyttää, kuinka monta näistä luvuista on parillisia.
 // Koska käytössä on prompt, tämä toimii selaimessa html-sivun kautta.
 
-let summa = 0;
 let laskuri = 0;
 let parillisetLuvut = 0;
 
 do {
   let syote = Number(prompt("Syötä luku"));
-  summa += syote;
   laskuri++;
-  if (syote % 2 === 0) {
+  if (syote % 2 == 0) {
     parillisetLuvut++;
   }
 } while (laskuri !== 20);

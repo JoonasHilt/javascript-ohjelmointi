e6f8894b7de14839618f5a30943a3cc3887b86db
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-04-18 13:51:38 +0300

    päivitetty kansiorakenne

--- js-perusteet/osa-3/03_harjoitukset/silmukat/01_silmukat.js
+++
@@ -1,7 +0,0 @@
-//Tee ohjelma, joka tulostaa kaikki parittomat numerot 1 ja 100 välillä. Eli 1, 3, 5, 7, 9, 11 jne.
-
-for (let number = 1; number <= 100; number++) {
-  if (number % 2 !== 0) {
-    console.log(number);
-  }
-}

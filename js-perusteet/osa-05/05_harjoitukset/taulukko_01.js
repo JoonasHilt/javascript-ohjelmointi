2f2370e2bc611f9b99e2aab82fad616f70bb78c3
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-04-18 14:57:45 +0300

    päivitetty päivän päätteeksi taulukko harjoitus

--- js-perusteet/osa-05/05_harjoitukset/taulukko_01.js
+++ js-perusteet/osa-05/05_harjoitukset/taulukko_01.js
@@ -29,12 +29,16 @@ console.log(teamMembers);
 
 // Harjoitus 6: Luo uusi taulukko, joka ei sisällä kahta ensimmäistä jäsentä.
 // Kirjoita koodisi tähän:
+teamMembers.splice(0, 2);
+console.log(teamMembers);
 
 // Harjoitus 7: Etsi "Miikka" indeksi taulukossa.
 // Kirjoita koodisi tähän:
+console.log(teamMembers.indexOf("Miikka"));
 
 // Harjoitus 8: Yritä löytää "Jaakko" indeksi (joka ei ole taulukossa).
 // Kirjoita koodisi tähän:
+console.log(teamMembers.indexOf("Jaakko"));
 
 // Harjoitus 9: Käytä `splice` poistaaksesi "Miikka" ja lisätäksesi "Karoliina" ja "Bettina" hänen tilalleen.
 // Kirjoita koodisi tähän:

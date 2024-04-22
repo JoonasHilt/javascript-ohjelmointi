b383f0ca44640cbacd69221683994e292f5cda74
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-04-22 12:24:59 +0300

    päivitetty ehtolause scriptiin

--- js-perusteet/osa-04/04_dom_harjoitukset/DOM-2/script.js
+++ js-perusteet/osa-04/04_dom_harjoitukset/DOM-2/script.js
@@ -14,6 +14,12 @@ function addFruitBtn() {
   const input = document.getElementById("fruitInput");
   const inputValue = input.value;
 
+  // tarkistaa jos syöttökenttä on tyhjä promptaa alertin ja lopettaa funktion toteutumisen jotta ei tule tyhjää lsita elementtiä
+  if (inputValue === "") {
+    alert("LISÄÄ HEDELMÄ LISTAAN!");
+    return;
+  }
+
   // tee uusi li element
   const newListItem = document.createElement("li");
   newListItem.textContent = inputValue;

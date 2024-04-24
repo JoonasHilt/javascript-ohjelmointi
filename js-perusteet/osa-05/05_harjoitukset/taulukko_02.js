79f58afe24537fc08f41bf73cf5962ee60978c69
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-04-24 10:35:49 +0300

    taulukko 2 päivitetty

--- js-perusteet/osa-05/05_harjoitukset/taulukko_02.js
+++ js-perusteet/osa-05/05_harjoitukset/taulukko_02.js
@@ -8,6 +8,17 @@ Odotettu tulos:
 "Taulukon pituus on suurempi tai yhtä suuri kuin 5."
 */
 // Kirjoita funktiosi tähän
+function myAlphabetLength(taulukko) {
+  const length = taulukko.length;
+  console.log("Taulukon myAlphabet pituus: ", length);
+  if (length < 5) {
+    console.log("Taulukon pituus on alle 5.");
+  } else {
+    console.log("Taulukon pituus on suurempi tai yhtä suuri kuin 5.");
+  }
+}
+//kutsutaan tällä funktio
+myAlphabetLength(myAlphabet);
 
 // Harjoitus 2: Käy läpi taulukko ja kirjaa jokainen alkio sen indeksillä
 const planets = ["Merkurius", "Venus", "Maa", "Mars", "Jupiter"];
@@ -19,6 +30,9 @@ Odotetut tulokset:
 "Planeetta: Jupiter, Indeksi: 4"
 */
 // Kirjoita koodisi tähän
+for (let i = 0; i < planets.length; i++) {
+  console.log(`Planeetta: ${planets[i]}, Indeksi ${i}`);
+}
 
 // Harjoitus 3: Kirjaa taulukon alkiot niiden tyyppeineen
 const wowDatatypes = [1, "text", false, null, undefined];
@@ -31,6 +45,11 @@ Odotetut tulokset:
 "Alkio: undefined, Indeksi: 4, Tyyppi: undefined"
 */
 // Kirjoita koodisi tähän
+for (let i = 0; i < wowDatatypes.length; i++) {
+  const item = wowDatatypes[i];
+  const type = typeof item;
+  console.log(`Alkio: ${wowDatatypes[i]}, Indeksi: ${i}, Tyyppi: ${type}`);
+}
 
 // Harjoitus 4: Kirjaa taulukon alkiot ilman silmukkaa
 let myArr = [1, 2, "One", true];
